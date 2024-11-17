c3409f8a471bd2a8fce44747dafea1d4280f3153
Wisnu Setia Budi <wisnusetiabudi09@gmail.com>
2024-11-17 21:56:30 +0800

    Update PengelolaanKontakFrame.java

--- src/PengelolaanKontakFrame.java
+++ src/PengelolaanKontakFrame.java
@@ -7,16 +7,8 @@ import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author ssiti
- */
+
 public class PengelolaanKontakFrame extends javax.swing.JFrame {
     private final DataBaseHelper dbHelper;
     /**
